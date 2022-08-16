4dc41de86aa9764b338d428557d07d29bca9baad
Nadim Mansuri <89566087+nadimmansuri2242@users.noreply.github.com>
2022-08-16 18:21:00 +0530

    Add new file : get user input

---
+++ userinput.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+input  = gets.chomp
+
+puts "What is your age?"
+input  = gets.chomp
+
+puts "What is your hobby?"
+input  = gets.chomp
+
+puts "What is your goal?"
+input  = gets.chomp
+puts "Thanks to tell me little about your self."
